71798989265fd85bcd094b6b762395efddecb288
Ali Haddadi <ali.haddadi154@gmail.com>
2022-05-15 17:39:53 +0430

    config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCqRx_dxUUSRJd23S0j8H5-VDkdLMK3iSo",
+    authDomain: "vue-chat-app-a3b2a.firebaseapp.com",
+    projectId: "vue-chat-app-a3b2a",
+    storageBucket: "vue-chat-app-a3b2a.appspot.com",
+    messagingSenderId: "438123257319",
+    appId: "1:438123257319:web:93d8ece485381817ccf142",
+    measurementId: "G-8NXHPEPG4N",
+  };
+  
